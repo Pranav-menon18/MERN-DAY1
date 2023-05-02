033d62d4af92d84fffdf183b015763c2a77c0140
Pranav-menon18 <132336564+Pranav-menon18@users.noreply.github.com>
2023-05-02 15:55:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,156 @@
+// console.log("hello world");
+// var let const
+// var a= 10;
+// console.log(a);
+// var a =20;
+// console.log(a)
+// {
+    // let b=23; 
+// console.log(b);
+// }
+// let b=34;
+// console.log(b);
+// let a=10;
+// let b=20;
+// let c=a+b;
+// let d=b-a;
+// let e=a*b;
+// let f=a/b;
+// let g=a%b;
+// console.log(c);
+// console.log(d);
+// console.log(e);
+// console.log(f);
+// console.log(g);
+// let a=10;
+// let b="10";
+// if(a<b){
+    // console.log(true)
+// }
+// else{
+    // console.log(false);
+// }
+// if(a==b){
+    // console.log(true)
+// }
+// else{
+    // console.log(false)
+// }
+// let f="tiya";
+// let j="stephen";
+// let FN=f+j;
+// console.log(FN);
+// console.log(`${f} and ${j}`);
+// let car ={
+//     model:"celerio",
+//     color:"white",
+//     price:100000
+// }
+// console.log(car);
+// console.log(car.color);
+// console.log(`My car is ${car.color} ${car.model}`);
+// let person = {
+//     name:"pranav",
+//     college:"christ",
+//     class:"cs",
+//     address:{
+//         housename:"xyz",
+//         street:"dggg",
+//     }
+// }
+// console.log(person);
+// console.log(person.name);
+// console.log(person.address.housename);
+// array
+// let fruits=["apple","mango","orange",10]
+// fruits[3]="grapes";
+// fruits.push("pineapple")
+// fruits.unshift("Drangon")
+// fruits.pop();
+// console.log(fruits);
+// console.log(fruits.length)
+
+// array of objects
+// let todo =[
+//     {id:1,
+//     task: "Buy book"
+//     },
+// {
+//     id:2,
+//     task: "hde"
+// },
+// {
+//     id:3,
+//     task: "hgj"
+// },
+// ]
+// console.log(todo);
+// console.log(todo[1]);
+// console.log(todo[1].task);
+// let students =[
+//     {name:"Arun",age:20},{name:"saya",age:22}
+// ]
+// console.log(students[1].name);
+// function
+// function addTwoNumbers(a, b) {
+//     console.log(a + b);
+// }
+
+// add = (a, b) => {
+//     console.log(a+b);
+// }
+// addTwoNumbers(10, 20);
+
+// add(20, 30);
+// arrow function
+// myfun =(a, b) =>{
+//     let c= a+b;
+//     if(c>10){
+//         console.log("greater than 10");
+//     }
+//     else{
+//         console.log("less than 10")
+//     }
+    
+// }
+// myfunc(3,5);
+// myfunc(10,1);
+
+// let numbers = [1,2,3,4,5,6,7,8,9,10];
+// console.log(numbers);
+
+// printarray = (array) => {
+//     for(let i=0; i<array.length; i++) {
+//         console.log(array[i]);
+//     }
+// }
+
+// printarray(numbers);
+let numbers = [1,2,3,4,5,6,7,8,9,10]
+console.log("while");
+myfucwhile = (a) =>{
+    var i=0;
+    while(i<a.length){
+        console.log(a[i])
+        i++;}
+    }
+myfncwhile(numbers)
+console.log("dowhile")
+myfncwhile = (a) =>{
+    var i=0;
+    do{
+        console.log(a[i])
+        i++;}
+        while(i<a.length)
+
+    }
+myfncdowhile(numbers)
+
+
+
+
+
+
+
+
+
